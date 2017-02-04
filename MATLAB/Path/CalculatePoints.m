d94c470ae9bb53cc41eec54a015ffe71dd0fdd03
fkamel <fabdelhamid@gmail.com>
2017-02-04 18:47:27 -0400

    Moved files around

--- MATLAB/Path/CalculatePoints.m
+++ MATLAB/Path/CalculatePoints.m
@@ -1,5 +1,14 @@
-function CalculatePoints (a, b, distance_between_points)
+function resulting_points=Cal0culatePoints (a, b, distance_meters)
 % CalculatePoints Calculates points along a path.
-    
+
+bearing_ba = CalculateBearing (b,a);
+resulting_points = [];
+c = a;
+x = a;
+while CalculateRange (x,a) < CalculateRange (b,a)
+    x = DisplacementToPoint (c, distance_meters, bearing_ba);
+    c = x;
+    resulting_points = [resulting_points; x];    
+end
 
 end
