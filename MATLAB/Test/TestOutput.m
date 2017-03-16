934f120300492301ad3fbe557f75217afbb344d7
fkamel <fabdelhamid@gmail.com>
2017-03-15 21:50:34 -0300

    Test path update

--- MATLAB/Test/TestOutput.m
+++ MATLAB/Test/TestOutput.m
@@ -1,15 +1,53 @@
 function TestOutput()
-    fid1 = fopen('output1.TXT','r'); 
+    fid1 = fopen('test_output.txt','r'); 
+    
+    figure;
+    all_coords = [];
+    all_times = [];
+    
     while ~feof(fid1)        
         line = fgets(fid1);
-        line = line(1:end-1)
+        line = line(1:end-1);
         
-        %A = sscanf(line,'%*[^,],%f,%f'); %# sscanf can read only numeric 
-        % data :(
-        %if A(2)<4.185 %# test the values
-        %    fprintf(fid2,'%s',line) %# write the line to the new file
-        %end
+        time = line;
         
+        line = fgets(fid1);
+        line = line(1:end-1);        
+        coords = line;
+        coords = strsplit(coords,',');
+       
+        lat = char(coords(1));
+        lon = char(coords(2));
+       
+        if strcmp (lat(end), 'S')
+            lat_sign = -1;
+        else
+            lat_sign = 1;
+        end % if
+        
+        if strcmp (lon(end), 'W')
+            lon_sign = -1;
+        else
+            lon_sign = 1;
+        end % if 
+        
+        lat = str2num(lat(1:end-1)) * lat_sign;
+        lon = str2num(lon(1:end-1)) * lon_sign;
+        
+               
+        line = fgets(fid1);
+        line = line(6:end-1);
+        
+        ypr = line;
+        ypr = strsplit(ypr,',');
+
+        time_s = str2num(time) / 1000;
+        
+        
+        all_coords = [all_coords; [lat, lon]];
+                
     end    
     fclose(fid1);
+    
+ 
 end
