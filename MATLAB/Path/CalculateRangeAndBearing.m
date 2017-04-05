ebdf9eacd63899c3bf395076ba95969fc86c1505
sebasb06 <Sebastien.boivin@dal.ca>
2017-04-05 19:12:35 -0300

    updates

--- MATLAB/Path/CalculateRangeAndBearing.m
+++ MATLAB/Path/CalculateRangeAndBearing.m
@@ -1,4 +1,4 @@
 function  [range,bearing] = CalculateRangeAndBearing (point2, point1)
     [range, bearing] = distance(point1(1),point1(2),point2(1),point2(2));
-    range = distdim(deg2nm(range),'nm','km');
+    range = distdim(deg2nm(range),'nm','km'); %%% what is the disttdim 
 end % 
